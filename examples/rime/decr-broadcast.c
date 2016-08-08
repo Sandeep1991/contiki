ba0fe64057cfb1df5a2aec1542a62272dc74bb84
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2016-08-08 12:02:22 -0700

    Add files via upload
    Substitution cipher (Caesar cipher) using broadcast example.

---
+++ examples/rime/decr-broadcast.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         Testing the broadcast layer in Rime
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+#include "net/rime.h"
+#include "random.h"
+
+#include "dev/button-sensor.h"
+
+#include "dev/leds.h"
+
+#include <stdio.h>
+#include <string.h>
+
+/*---------------------------------------------------------------------------*/
+PROCESS(example_broadcast_process, "Broadcast example");
+AUTOSTART_PROCESSES(&example_broadcast_process);
+/*---------------------------------------------------------------------------*/
+//SKV --commits for substitution cipher//
+
+
+char* subs(char *s, int k)
+{
+	int i=0;	   
+	for(;i<strlen(s);i++)
+	{
+		int m = (s[i]-'a'-k)%26;
+		if(m<0) m = m + 26;
+		s[i] =m + 'a';
+	}	
+	return s;
+}
+
+static void
+broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
+{
+  printf("broadcast message received from %d.%d: '%s'\n",
+         from->u8[0], from->u8[1], subs((char *)packetbuf_dataptr(),5));//sending the data in pack buffer to function for decryption -SKV
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(example_broadcast_process, ev, data)
+{
+  static struct etimer et;
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+  PROCESS_BEGIN();
+
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
+  while(1) {
+
+    /* Delay 2-4 seconds */
+    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+    //Type the message to be encrypted 
+    //char msg[] = "zand";
+    //int msg_len = 7;
+    //Set key
+    //int key = 5;
+    //Invoke substitution function
+    
+    //packetbuf_copyfrom(subs(msg,key), 5);
+    broadcast_send(&broadcast);
+    printf("broadcast message sent\n");
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
